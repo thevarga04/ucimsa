e8190136e9bc2e21a45ace182a1b09ee10e3e775
Pali Varga <pavol.varga2@ebcont.com>
2023-04-23 21:34:09 +0200

    Provide List of Texts

--- src/main/webapp/resources/js/texts.js
+++ src/main/webapp/resources/js/texts.js
@@ -74,63 +74,64 @@ function legend() {
 
 function textsAsCards(texts) {
   for (let text of texts) {
-    textAsCard(text);
+    textCard(text.id, text.textname, entries(text.sentences));
   }
 }
 
 // Textname :: Sentences/Entries :: Learn :: Stats :: Share/Private (Delete text when editing it)
-function textAsCard(text) {
-  textCard(text.id, text.textname, entries(text.sentences));
+function textCard(id, textname, entries) {
+  let divCard = document.createElement("div");
+  divCard.setAttribute("class", "d-flex card shadow border-left-heap");
+
+  let divRow = document.createElement("div");
+  divRow.setAttribute("class", "row card-body-list");
+
+  let divColName = textNameElement(id, textname);
+  let divColEntries = entriesElement(entries);
+  let divColLearn = textLearnElement(id);
+  let divColStats = textStatsElement(id);
+
+  divRow.append(divColName, divColEntries, divColLearn, divColStats);
+  divCard.append(divRow);
+  cardBody.append(divCard);
 }
 
-function textCard(id, textname, entries) {
-  let type = "HEAP";
+function entries(sentences) {
+  return sentences.split(/\r?\n/).length;
+}
 
-  let div1 = document.createElement("div");
-  div1.setAttribute("id", "card_" + id);
-  div1.setAttribute("class", "mb-1");
-
-  let div2 = document.createElement("div");
-  div2.setAttribute("class", "card shadow border-left-heap");
-
-  let div3 = document.createElement("div");
-  div3.setAttribute("class", "card-body-list");
-
-  let div4 = document.createElement("div");
-  div4.setAttribute("class", "row align-items-center");
-
-  let div5 = document.createElement("div");
-  div5.setAttribute("class", "col");
-
-  let div6 = document.createElement("div");
-  div6.setAttribute("class", "row align-items-center justify-content-between");
-
-  // Icon, Testname
-  let div7 = document.createElement("div");
-  div7.setAttribute("class", "col-auto");
-
-  let div8 = document.createElement("div");
-  div8.setAttribute("class", "h6 mb-0 font-weight-bold");
-
+function textNameElement(id, textname) {
+  let divColName = document.createElement("div");
+  divColName.setAttribute("class", "col-auto flex-grow-1");
   let nameLink = document.createElement("a");
   nameLink.title = "View or Edit this Test";
-  nameLink.href = "/api/texts/" + id + "?type=" + type;
+  nameLink.href = "/api/texts/" + id + "?type=HEAP";
   let nameIcon = document.createElement("i");
   nameIcon.setAttribute("class", "fas fa-edit me-2");
   let nameText = document.createTextNode(textname);
   nameLink.append(nameIcon, nameText);
+  divColName.append(nameLink);
+  return divColName;
+}
 
-  // Sentences/Entries
-  let div7Entries = document.createElement("div");
-  div7Entries.setAttribute("class", "col-auto");
-  let div7Span = document.createElement("span");
-  div7Span.setAttribute("class", "badge badge-secondary badge-pill");
-  div7Span.title = type === "HEAP" ? "Sentences" : "Entries";
-  div7Span.setAttribute("style", "cursor: pointer; font-size: 1rem" + (type === "HEAP" ? "; color: #E8CDA8" : ""));
-  div7Span.append(entries);
-  div7Entries.append(div7Span);
+function entriesElement(entries) {
+  let divColEntries = document.createElement("div");
+  divColEntries.setAttribute("class", "col-auto");
+  let divEntries = document.createElement("div");
+  divEntries.setAttribute("class", "col-auto");
+  let divSpan = document.createElement("span");
+  divSpan.setAttribute("class", "badge badge-secondary badge-pill");
+  divSpan.title = "Sentences";
+  divSpan.setAttribute("style", "cursor: pointer; font-size: 1rem; color: #E8CDA8");
+  divSpan.append(entries);
+  divEntries.append(divSpan);
+  divColEntries.append(divEntries);
+  return divColEntries;
+}
 
-  // Learn
+function textLearnElement(id) {
+  let divColLearn = document.createElement("div");
+  divColLearn.setAttribute("class", "col-auto");
   let divLearn = document.createElement("div");
   divLearn.setAttribute("class", "col-auto");
   let learnLink = document.createElement("a");
@@ -143,8 +144,13 @@ function textCard(id, textname, entries) {
   let learnText = document.createTextNode("Learn");
   learnLink.append(learnIcon, learnText);
   divLearn.append(learnLink);
+  divColLearn.append(divLearn);
+  return divColLearn;
+}
 
-  // Stats
+function textStatsElement(id) {
+  let divColStats = document.createElement("div");
+  divColStats.setAttribute("class", "col-auto");
   let divStats = document.createElement("div");
   divStats.setAttribute("class", "col-auto");
   let statsLink = document.createElement("a");
@@ -157,34 +163,24 @@ function textCard(id, textname, entries) {
   let statsText = document.createTextNode("Stats");
   statsLink.append(statsIcon, statsText);
   divStats.append(statsLink);
-
-  // // Delete is irreversible operation, delete text when editing
-  // let divDelete = document.createElement("div");
-  // divDelete.setAttribute("class", "col-auto");
-  // let delete_button = document.createElement("button");
-  // delete_button.setAttribute("style", "width: 90px");
-  // delete_button.setAttribute("class", "btn btn-outline-danger btn-sm");
-  // delete_button.title = "Delete this text permanently.";
-  // delete_button.onclick = function () { deleteText(id) };
-  // let delete_icon = document.createElement("i");
-  // delete_icon.setAttribute("class", "far fa-trash-alt me-2");
-  // let delete_text = document.createTextNode("Delete");
-  // delete_button.append(delete_icon, delete_text);
-  // divDelete.append(delete_button);
-
-  div8.append(nameLink);
-  div7.append(div8);
-  div6.append(div7, div7Entries);
-  div5.append(div6);
-  div4.append(div5, divLearn, divStats);
-  div3.append(div4);
-  div2.append(div3);
-  div1.append(div2);
-  cardBody.append(div1);
+  divColStats.append(divStats);
+  return divColStats;
 }
 
-function entries(sentences) {
-  return sentences.split(/\r?\n/).length;
+// Delete is irreversible operation, delete text when editing
+function textDeleteElement(id) {
+  let divDelete = document.createElement("div");
+  divDelete.setAttribute("class", "col-auto");
+  let delete_button = document.createElement("button");
+  delete_button.setAttribute("style", "width: 90px");
+  delete_button.setAttribute("class", "btn btn-outline-danger btn-sm");
+  delete_button.title = "Delete this text permanently.";
+  delete_button.onclick = function () { deleteText(id) };
+  let delete_icon = document.createElement("i");
+  delete_icon.setAttribute("class", "far fa-trash-alt me-2");
+  let delete_text = document.createTextNode("Delete");
+  delete_button.append(delete_icon, delete_text);
+  divDelete.append(delete_button);
 }
 
 function deleteText(id) {
